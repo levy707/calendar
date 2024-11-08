933d367d7e7371af7ff7cc239e4af63874b3218e
levy707 <levy707@users.noreply.github.com>
2024-11-08 16:01:03 +0100

    Component extraction from RangePicker.vue (#3)
    * Component extraction from RangePicker.vue
    
    * Updated CHANGELOG.md
    
    ---------
    
    Co-authored-by: levy707 <levy707>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
+### [0.3.0](https://github.com/levy707/calendar/compare/0.2.1...0.3.0) (2024-11-08)
+
+### Features
+
+-   component extraction from RangePicker.vue
+
 
 ### [0.2.1](https://github.com/levy707/calendar/compare/0.2.0...0.2.1) (2024-10-31)
 
--- tests/unit/RangePicker.spec.js
+++ tests/unit/RangePicker.spec.js
@@ -1,64 +1,31 @@
 import { shallowMount } from '@vue/test-utils';
 
 // Components
-import RangePicker from '@/components/FormsElements/RangePicker.vue';
+import RangePicker from '@/components/atoms/RangePicker/RangePicker.vue';
 
 // Directives
 import { clickOutside } from '@/directives/clickOutside.js';
 
-describe('RangePicker.vue - Navigation', () => {
+describe('RangePicker.vue', () => {
     let wrapper;
 
-    function mountComponent(propsData = {}) {
-        wrapper = shallowMount(RangePicker, {
+    beforeEach(() => {
+        jest.useFakeTimers();
+    });
+
+    const factory = props => {
+        return shallowMount(RangePicker, {
+            propsData: {
+                ...props,
+            },
             directives: { clickOutside },
-            propsData,
         });
-        wrapper.setData({
-            today: new Date('2020.12.10'),
-            currentMonth: new Date('2020.12.10').getMonth(),
-            currentYear: new Date('2020.12.10').getFullYear(),
-        });
-    }
+    };
 
-    beforeEach(() => mountComponent());
-
-    jest.useFakeTimers();
     it('Show calendar', () => {
-        wrapper.find('.range-picker__block').trigger('click');
+        wrapper = factory();
+        wrapper.find('.range-picker-block').trigger('click');
         jest.advanceTimersByTime(0);
         expect(wrapper.vm.isCalendarVisible).toBe(true);
-    });
-
-    it('Show next month', () => {
-        wrapper.find('.range-picker__block').trigger('click');
-        jest.advanceTimersByTime(0);
-        wrapper.vm.$nextTick(() => {
-            wrapper.find('.calendar__header-navigation--next').trigger('click');
-            expect(wrapper.vm.currentMonthName).toBe('January');
-        });
-    });
-
-    it('Show previous month', () => {
-        wrapper.setData({
-            today: new Date('2020.11.10'),
-            currentMonth: new Date('2020.12.10').getMonth(),
-            currentYear: new Date('2020.12.10').getFullYear(),
-        });
-        wrapper.find('.range-picker__block').trigger('click');
-        jest.advanceTimersByTime(0);
-        wrapper.vm.$nextTick(() => {
-            wrapper.find('.calendar__header-navigation--prev').trigger('click');
-            expect(wrapper.vm.currentMonthName).toBe('November');
-        });
-    });
-
-    it('Blocked navigation to previous month', () => {
-        wrapper.find('.range-picker__block').trigger('click');
-        jest.advanceTimersByTime(0);
-        wrapper.vm.$nextTick(() => {
-            wrapper.find('.calendar__header-navigation--prev').trigger('click');
-            expect(wrapper.vm.currentMonthName).toBe('December');
-        });
     });
 });
